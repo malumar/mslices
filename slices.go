0bb61ed85ccf90ba0c8298805f2f32f7bfd52cd2
mar.maluszczak@gmail.com <297255+malumar@users.noreply.github.com>
2024-10-25 13:16:01 +0200

    support arrays in program flags

--- slices.go
+++ slices.go
@@ -1,5 +1,7 @@
 package slices
 
+import "fmt"
+
 // Unique remove duplicated values from array
 // @return new table with unique values
 func Unique[T comparable](ar []T) []T {
@@ -41,4 +43,21 @@ func Filter[T any](ar []T, keep func(val T) bool) []T {
 		}
 	}
 	return ret
+}
+
+// SliceOfFlag array support for variables in program flags
+// np.
+// var list sliceOfFlag[string]
+// flag.Var(&list, "item", "additional items")
+type SliceOfFlag[T any] []T
+
+// String is an implementation of the flag.Value interface
+func (i *SliceOfFlag[T]) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+// Set is an implementation of the flag.Value interface
+func (i *SliceOfFlag[T]) Set(value T) error {
+	*i = append(*i, value)
+	return nil
 }
